6c7fa24678b79b3b6e0c1f8b94bb56914fa90e8a
boyd ndonga <boydbugle@gmail.com>
2018-03-02 11:50:23 +0300

    creates form storage

--- pycode/loginWwithSession.py
+++ pycode/loginWwithSession.py
@@ -2,5 +2,13 @@ import cgitb; cgitb.enable()
 import sys, os, cgi, pickle
 from cookie import SimpleCookie
 
+
+class form(dict):
+    def __init__(self, fields):
+        dict.__init__(self,fields):
+        for k, v in self.items(): self[k] = v.value
+
+form = form(cgi.FieldStorage())
+
 if __name__=='__main__':
     main()
